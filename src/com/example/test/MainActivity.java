5566efbfbabc7d354c7ecdfaa590095c8982bf13
mandmLeee <526450246@qq.com>
2015-04-30 19:46:10 +0800

    code

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -10,16 +13,19 @@ import java.net.URL;
 import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Movie;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-	protected static final String IMAGE_URL = "http://img.blog.csdn.net/20150410135837339";
-	// protected static final String IMAGE_URL =
-	// "http://img.blog.csdn.net/20150310123909933";
+	// protected static final String IMAGE_URL = "http://img.blog.csdn.net/20150410135837339";
+	protected static final String IMAGE_URL ="http://img.blog.csdn.net/20150310123909933";
 
 	protected static final String TAG = "MainActivity";
 	private GifImageView myGifImageView;
@@ -29,10 +35,10 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		myGifImageView = (GifImageView) findViewById(R.id.myGifView);
-		new AsyncTask<Void, Void, GifDrawable>() {
+		new AsyncTask<Void, Void, byte[]>() {
 
 			@Override
-			protected GifDrawable doInBackground(Void... params) {
+			protected byte[] doInBackground(Void... params) {
 				byte[] gifbyte = null;
 				HttpURLConnection conn = null;
 				try {
@@ -61,41 +67,44 @@ public class MainActivity extends Activity {
 				}
 
 				// 写入文件
-				/*FileOutputStream fos = null;
-				try {
 
-					File root = Environment.getExternalStorageDirectory();
-					File myFile = new File(root, "test.gif");
-					Log.v(TAG, myFile.getAbsolutePath());
-					fos = new FileOutputStream(myFile);
-					fos.write(gifbyte);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				} finally {
-					if (fos != null) {
-						try {
-							fos.close();
-						} catch (IOException e) {
+				/*
+				 * FileOutputStream fos = null; try {
+				 * 
+				 * File root = Environment.getExternalStorageDirectory(); File
+				 * myFile = new File(root, "test.jpg"); Log.v(TAG,
+				 * myFile.getAbsolutePath()); fos = new
+				 * FileOutputStream(myFile); fos.write(gifbyte); } catch
+				 * (FileNotFoundException e) { e.printStackTrace(); } catch
+				 * (IOException e) { e.printStackTrace(); } finally { if (fos !=
+				 * null) { try { fos.close(); } catch (IOException e) {
+				 * 
+				 * e.printStackTrace(); } } }
+				 */
 
-							e.printStackTrace();
-						}
-					}
-				}*/
+				return gifbyte;
 
-				GifDrawable gifDrawable = null;
-				try {
-					gifDrawable = new GifDrawable(gifbyte);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				return gifDrawable;
 			}
 
-			protected void onPostExecute(GifDrawable drawable) {
+			protected void onPostExecute(byte[] gifbyte) {
 
-				myGifImageView.setImageDrawable(drawable);
+				// 判断是否是gif图
+				Movie gif = Movie.decodeByteArray(gifbyte, 0, gifbyte.length);
+				if (gif != null) {
+					Log.v(TAG, "是gif图片");
+					GifDrawable gifDrawable = null;
+					try {
+						gifDrawable = new GifDrawable(gifbyte);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					myGifImageView.setImageDrawable(gifDrawable);
+				} else {
+					Bitmap gifBitmap = BitmapFactory.decodeByteArray(gifbyte,
+							0, gifbyte.length);
+					myGifImageView.setImageBitmap(gifBitmap);
+				}
+
 			};
 
 		}.execute();
